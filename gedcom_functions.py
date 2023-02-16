e78806ef3cf4bbeb6de1e9879ee38be79f36e795
Leonel Trubbo <leotrubbo@gmail.com>
2023-02-16 15:27:43 -0500

    added tests and functions for large age diffs and orphans
    still need to touch up the functions and the tests

--- gedcom_functions.py
+++ gedcom_functions.py
@@ -12,4 +12,35 @@ def birthBeforeDeath(birth, death):
     else:
         # If death after birth.
         return True
+# Lists all orphaned children when passed
+# a list of children (under 18).
+def listOrphans(children):
+    orphans = []
+    for i in children: 
+        age = parser.parse(children[i].age)
+        #Child is not orphan if greater than 17 years of age
+        if (age > 17):
+            continue
+        else:
+            #Check if both parents are dead
+            if (((children[i].parent1.dead) and (children[i].parent2.dead)) != True):
+                continue
+            #Append to orphan list
+            orphans += children[i]
+    return orphans
+# List all couples who were married when the older spouse was
+# more than twice as old as the younger spouse
+def listLargeAgeDifferences(couples):
+    ageDiff = []
+    for i in couples: 
+        # Use marriage date - birth date to figure out ages of the spouses
+        # For simplicity use marriage year and birth year
+        marriage = parser.parse(couples[i].marriageDate)
+        spouse1_birth = parser.parse(couples[i].spouse1.birth)
+        spouse2_birth = parser.parse(couples[i].spouse.birth)
+        # AAM = Age at Marriage
+        spouse1_AAM = marriage - spouse1_birth
+        spouse2_AAM = marriage - spouse2_birth
 
+
+         
--- gedcom_functions_tests.py
+++ gedcom_functions_tests.py
@@ -30,6 +30,56 @@ class testGEDCOM(unittest.TestCase):
         result = gedcom_functions.birthBeforeDeath('14 OCT 1912', '13 OCT 1912')
         self.assertFalse(result)
 
+    # Should return an empty list.
+    def test1_listOrphans(self):
+        result = gedcom_functions.listOrphans([])
+        self.assertEqual(result)
+
+        # Should return...
+    def test2_listOrphans(self):
+        result = gedcom_functions.listOrphans([])
+        self.assertEqual(result)
+
+        # Should return...
+    def test3_listOrphans(self):
+        result = gedcom_functions.listOrphans([])
+        self.assertEqual(result)
+
+         # Should return...
+    def test4_listOrphans(self):
+        result = gedcom_functions.listOrphans([])
+        self.assertEqual(result)
+
+         # Should return...
+    def test5_listOrphans(self):
+        result = gedcom_functions.listOrphans([])
+        self.assertEqual(result)
+
+    # Should return an empty list.
+    def test1_listLargeAgeDifferences(self):
+        result = gedcom_functions.listLargeAgeDifferences([])
+        self.assertEqual(result)
+
+        # Should return...
+    def test2_listLargeAgeDifferences(self):
+        result = gedcom_functions.listLargeAgeDifferences([])
+        self.assertEqual(result)
+
+        # Should return...
+    def test3_listLargeAgeDifferences(self):
+        result = gedcom_functions.listLargeAgeDifferences([])
+        self.assertEqual(result)
+
+         # Should return...
+    def test4_listLargeAgeDifferences(self):
+        result = gedcom_functions.listLargeAgeDifferences([])
+        self.assertEqual(result)
+        
+         # Should return...
+    def test5_listLargeAgeDifferences(self):
+        result = gedcom_functions.listLargeAgeDifferences([])
+        self.assertEqual(result)
+
 # Enables us to call test file like python file.
 if __name__ == '__main__':
     unittest.main()
