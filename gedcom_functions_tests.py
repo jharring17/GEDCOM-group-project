9f505ed36bbecafb65fc2cd359ec59b2b18ab488
Jack Harrington <jackharrington1117@gmail.com>
2023-02-16 20:53:31 -0500

    Added tests for birth before marriage.

--- gedcom_functions_tests.py
+++ gedcom_functions_tests.py
@@ -30,6 +30,31 @@ class testGEDCOM(unittest.TestCase):
         result = gedcom_functions.birthBeforeDeath('14 OCT 1912', '13 OCT 1912')
         self.assertFalse(result)
 
+    # Should evaluate to True.
+    def test1_birthBeforeMarriage(self):
+        result = gedcom_functions.birthBeforeMarriage(["@F2@", "5 JUL 1945", "NA", "@I6@",  "Arthur /Meumann/", "@I7@", "Lori /Meumann/",['@I2@', '@I8@']], ["@I6@", "Arthur /Meumann/", "M", "6 DEC 1922", "101", "False", "3 JAN 1999", "NA", "@F5@"])
+        self.assertTrue(result)
+
+    # Should evaluate to True.
+    def test2_birthBeforeMarriage(self):
+        result = gedcom_functions.birthBeforeMarriage(["@F2@", "5 JUL 1945", "NA", "@I6@",  "Arthur /Meumann/", "@I7@", "Lori /Meumann/",['@I2@', '@I8@']], ["@I7@", "Lori /Meumann/", "F", "6 DEC 1922", "101", "False", "3 JAN 1999", "NA", "@F5@"])
+        self.assertTrue(result)
+
+    # Should evaluate to False.
+    def test3_birthBeforeMarriage(self):
+        result = gedcom_functions.birthBeforeMarriage(["@F2@", "5 JUL 1945", "NA", "@I6@",  "Arthur /Meumann/", "@I7@", "Lori /Meumann/",['@I2@', '@I8@']], ["@I2@", "Brad /Meumann/", "M", "28 JUN 1961", "61", "True", "NA", "@F2@", "@F1@"])
+        self.assertEqual(result, 'Error: Individual provided not in family.')
+
+    # Should evaluate to False.
+    def test4_birthBeforeMarriage(self):
+        result = gedcom_functions.birthBeforeMarriage(["@F2@", "5 JUL 1945", "NA", "@I6@",  "Arthur /Meumann/", "@I7@", "Lori /Meumann/",['@I2@', '@I8@']], ["@I3@", "Sally /Go/", "F", "8 FEB 1961", "62", "True", "NA", "@F3@", "@F1@"])
+        self.assertEqual(result, 'Error: Individual provided not in family.')
+
+    # Should evaluate to False.
+    def test5_birthBeforeMarriage(self):
+        result = gedcom_functions.birthBeforeMarriage(["@F2@", "5 JUL 1945", "NA", "@I6@",  "Arthur /Meumann/", "@I7@", "Lori /Meumann/",['@I2@', '@I8@']], ["@I14@", "Jack", "M", "6 JAN 1968", "55", "True", "3 JAN 1999", "NA", "@F5@"])
+        self.assertEqual(result, 'Error: Individual provided not in family.')
+    
     # Should return an empty list.
     def test1_listOrphans(self):
         result = gedcom_functions.listOrphans([])
